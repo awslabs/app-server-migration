d5739c40046d0883ae1b70642da600e72eedc935
Mohd Feroz Baig <baigmohd@amazon.com>
2022-06-22 18:19:48 +0530

    Fix comments in arangoDB shell script

--- arangoDB.sh
+++ arangoDB.sh
@@ -9,15 +9,15 @@ set -o errexit
 # Pulls arangodb docker image and runs it in the foreground on port 8529
 #
 # USAGE:
-# bash arangodb.sh
+# bash arangoDB.sh
 
 ## docker
 if ! sudo docker info > /dev/null 2>&1; then
   echo "This script uses docker, and it isn't running - please start docker and try again!"
   exit 1
 fi
-# pull arangodb docker image and tag it
-##### Running in-memory graph database ArangoDB
+
+# Running in-memory graph database ArangoDB
 
 sudo docker run  -d -p 8529:8529 -e ARANGO_ROOT_PASSWORD=openSesame arangodb/arangodb:3.8.3
 
